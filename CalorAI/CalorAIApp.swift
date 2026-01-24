e146b3a0b952869e1fb6efd8b48b2946791bc474
Kevin Noh <119831259+Kn0824@users.noreply.github.com>
2026-01-24 17:37:20 +0900

    Initial Commit

---
+++ CalorAI/CalorAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorAIApp.swift
+//  CalorAI
+//
+//  Created by Gyu-Min Noh on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct CalorAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
